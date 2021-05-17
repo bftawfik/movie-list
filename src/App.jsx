cb638feefc4780678da20de1a3c5711756ac817c
Bishoy Farouk <bftawfik@gmail.com>
2021-05-17 20:42:09 +0200

    add home to app

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+import Home from "./Pages/Home/Home";
+
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Home />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
