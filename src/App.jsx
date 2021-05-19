3be1b515d80dffa3f12cdf40b2bcc0fec9e6c8ea
Bishoy Farouk <bftawfik@gmail.com>
2021-05-19 13:34:03 +0200

    add fonts to app

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Home from "./Pages/Home/Home";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 
+import "./App.scss";
+
 function App() {
   return (
     <React.Fragment>
