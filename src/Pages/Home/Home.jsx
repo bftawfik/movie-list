42745ed8e910ea3e8be252455477d24762630103
Bishoy Farouk <bftawfik@gmail.com>
2021-05-19 23:57:11 +0200

    show mock data in home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
 import FulscrnWrpr from "../../Components/FulscrnWrpr/FulscrnWrpr";
+import ItemBox from "../../Components/ItemBox/ItemBox";
 
 import * as styles from "./Home.module.scss";
 
+import data from "../../Mock/movie";
+
 const Home = () => {
+  const { results } = data;
   return (
     <FulscrnWrpr className={styles.Home} containerClassName={styles.container}>
-      Home
+      <div className={styles.itemsContainer}>
+        {results.map((item) => (
+          <ItemBox key={item.id} item={item} />
+        ))}
+      </div>
     </FulscrnWrpr>
   );
 };
