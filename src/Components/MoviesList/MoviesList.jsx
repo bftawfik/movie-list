f6d831110cb5bc4211f5e67f0f97bfa8c2b5652a
Bishoy Farouk <bftawfik@gmail.com>
2021-05-20 13:52:00 +0200

    add SectionHeader

--- src/Components/MoviesList/MoviesList.jsx
+++ src/Components/MoviesList/MoviesList.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-import ItemBox from "../../Components/ItemBox/ItemBox";
+import SectionHeader from "../SectionHeader/SectionHeader";
+import ItemBox from "../ItemBox/ItemBox";
 
 import * as styles from "./MoviesList.module.scss";
 
 const MoviesList = ({ pageNo, totalPages, totalMovies, loadedMovies }) => {
   return (
     <div className={styles.MoviesList}>
+      <SectionHeader title="title" />
       {loadedMovies.map((item) => (
         <ItemBox key={item.id} item={item} />
       ))}
---
+++ src/Components/SectionHeader/SectionHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { joinClassesWithSpace } from "../../Helpers/helperFunctions";
+
+import * as styles from "./SectionHeader.module.scss";
+
+const SectionHeader = ({ title, className }) => {
+  return (
+    <div className={joinClassesWithSpace(styles.SectionTitles, className)}>
+      <h2 className={styles.title}>{title}</h2>
+    </div>
+  );
+};
+
+export default SectionHeader;
