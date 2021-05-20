bd9c5c90e1885aa6a9d012e5503b74f975e2c9b6
Bishoy Farouk <bftawfik@gmail.com>
2021-05-20 23:32:48 +0200

    add needed mocks

--- src/Mock/home.js
+++ src/Mock/home.js
@@ -1,6 +1,11 @@
 const home = {
   allMovies: {
     title: "All Movies",
+    emptyListMsg: "No Movies To Show !",
+  },
+  myMovies: {
+    title: "My Movies",
+    emptyListMsg: "No Movies To Show !",
   },
 };
 
