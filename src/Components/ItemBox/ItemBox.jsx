cd0d29208f3bf16cfc7f99a8986b5d31e5c890a0
Bishoy Farouk <bftawfik@gmail.com>
2021-05-20 11:26:25 +0200

    use LinesEllipsis in ItemBox

--- src/Components/ItemBox/ItemBox.jsx
+++ src/Components/ItemBox/ItemBox.jsx
@@ -1,18 +1,37 @@
 import React from "react";
+import LinesEllipsis from "react-lines-ellipsis";
 
 import Data from "../../Services/Data";
 
 import * as styles from "./ItemBox.module.scss";
 
-const ItemBox = ({ item: { title, poster_path } }) => {
+const ItemBox = ({ item: { title, poster_path, release_date, overview } }) => {
   return (
     <div className={styles.ItemBox}>
+      <p className={styles.date}>
+        {/* <span>Realease Date:</span> */}
+        <span>{Data.getProperDate(release_date)}</span>
+      </p>
       <div className={styles.imageContainer}>
         <img src={Data.getProperImageUrl(poster_path)} />
       </div>
-      <h3>{title}</h3>
-      <p>date</p>
-      <p>overview</p>
+      <LinesEllipsis
+        component="h3"
+        ellipsis="..."
+        maxLine="2"
+        basedOn={"words"}
+        text={title || ""}
+        className={styles.title}
+      />
+
+      <LinesEllipsis
+        component="p"
+        ellipsis="..."
+        maxLine="3"
+        basedOn={"words"}
+        text={overview || ""}
+        className={styles.overview}
+      />
     </div>
   );
 };
