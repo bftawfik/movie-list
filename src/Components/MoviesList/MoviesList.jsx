ce798c2910fe8b7222a929d5b0ec73a24d80cf5b
Bishoy Farouk <bftawfik@gmail.com>
2021-05-20 23:32:32 +0200

    fix movies list to use it as my list

--- src/Components/MoviesList/MoviesList.jsx
+++ src/Components/MoviesList/MoviesList.jsx
@@ -5,55 +5,72 @@ import SectionHeader from "../SectionHeader/SectionHeader";
 import ItemBox from "../ItemBox/ItemBox";
 import BounceLoader from "../Spinners/BounceLoader/BounceLoader";
 
+import { joinClassesWithSpace } from "../../Helpers/helperFunctions";
+
 import * as styles from "./MoviesList.module.scss";
 
 const MoviesList = ({
   title,
+  emptyListMsg,
+  totalMovies,
   loadedMovies,
   loadingMore,
   rechedEnd,
   askForMore,
+  children,
 }) => {
   return (
-    <div className={styles.MoviesList}>
-      <SectionHeader title={title} />
+    <div
+      className={joinClassesWithSpace(
+        styles.MoviesList,
+        askForMore && styles.dynamicList
+      )}
+    >
+      <SectionHeader title={title}>
+        <p className={styles.totalMovies}>{`${totalMovies} Movies`}</p>
+      </SectionHeader>
+      {totalMovies === 0 && (
+        <div className={styles.emptyListMsg}>{emptyListMsg}</div>
+      )}
       <div className={styles.moviesContainer}>
         {loadedMovies.map((item) => (
           <ItemBox key={item.id} item={item} />
         ))}
       </div>
-      <div className={styles.sectionFooter}>
-        <InView
-          threshold={1}
-          onChange={(inView, entry) => {
-            if (inView && !loadingMore) {
-              askForMore();
-            }
-          }}
-        >
-          {({ inView, ref }) => {
-            return loadingMore ? (
-              <BounceLoader
-                topMsg="Loading..."
-                bottomMsg="Please Waiat."
-                ref={ref}
-              />
-            ) : rechedEnd ? (
-              <p className={styles.theEnd} ref={ref}>
-                The End
-              </p>
-            ) : (
-              <button
-                ref={ref}
-                className={styles.askForMore}
-                onClick={askForMore}
-              >
-                Show more
-              </button>
-            );
-          }}
-        </InView>
-      </div>
+      {askForMore && (
+        <div className={styles.sectionFooter}>
+          <InView
+            threshold={1}
+            onChange={(inView, entry) => {
+              if (inView && !loadingMore && askForMore) {
+                askForMore();
+              }
+            }}
+          >
+            {({ inView, ref }) => {
+              return loadingMore ? (
+                <BounceLoader
+                  topMsg="Loading..."
+                  bottomMsg="Please Waiat."
+                  ref={ref}
+                />
+              ) : rechedEnd ? (
+                <p className={styles.theEnd} ref={ref}>
+                  The End
+                </p>
+              ) : (
+                <button
+                  ref={ref}
+                  className={styles.askForMore}
+                  onClick={askForMore && askForMore}
+                >
+                  Show more
+                </button>
+              );
+            }}
+          </InView>
+        </div>
+      )}
     </div>
   );
 };
--- src/Components/SectionHeader/SectionHeader.jsx
+++ src/Components/SectionHeader/SectionHeader.jsx
@@ -4,10 +4,11 @@ import { joinClassesWithSpace } from "../../Helpers/helperFunctions";
 
 import * as styles from "./SectionHeader.module.scss";
 
-const SectionHeader = ({ title, className }) => {
+const SectionHeader = ({ title, className, children }) => {
   return (
     <div className={joinClassesWithSpace(styles.SectionHeader, className)}>
       <h2 className={styles.title}>{title}</h2>
+      <div className={styles.childrenContainer}>{children}</div>
     </div>
   );
 };
