3112ab210fb8888b15429c142a05ff0d0e04cb08
Bishoy Farouk <bftawfik@gmail.com>
2021-05-20 11:25:42 +0200

    add getProperDate helper

--- src/Services/Data.js
+++ src/Services/Data.js
@@ -1,8 +1,32 @@
+import data from "../Mock/movie";
+
 const domainUrl = "https://image.tmdb.org/t/p/original/";
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const getProperImageUrl = (relUrl) => domainUrl.concat(relUrl);
 
+const getProperDate = (dateStr) => {
+  const date = new Date(dateStr);
+  // console.log();
+  return `${monthNames[date.getMonth()]}-${date.getFullYear()}`;
+};
+
 const Data = () => ({
   getProperImageUrl: getProperImageUrl,
+  getProperDate: getProperDate,
 });
 export default Data();
