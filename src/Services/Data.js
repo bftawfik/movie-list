5fcbda4f54a0b405e62f91e5a9b18db7ce937483
Bishoy Farouk <bftawfik@gmail.com>
2021-05-20 13:14:16 +0200

    add loadMovies function

--- src/Services/Data.js
+++ src/Services/Data.js
@@ -1,4 +1,4 @@
-import data from "../Mock/movie";
+import axios from "axios";
 
 const domainUrl = "https://image.tmdb.org/t/p/original/";
 
@@ -21,12 +21,26 @@ const getProperImageUrl = (relUrl) => domainUrl.concat(relUrl);
 
 const getProperDate = (dateStr) => {
   const date = new Date(dateStr);
-  // console.log();
   return `${monthNames[date.getMonth()]}-${date.getFullYear()}`;
+};
+
+const loadMovies = async (
+  pageNo = 1,
+  language = "en-US",
+  listName = "popular"
+) => {
+  return await axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${listName}?api_key=${process.env.REACT_APP_THEMOVIEDB_API_KEY}&language=${language}&page=${pageNo}`
+    )
+    .then((res) => {
+      return res.data;
+    });
 };
 
 const Data = () => ({
   getProperImageUrl: getProperImageUrl,
   getProperDate: getProperDate,
+  loadMovies: loadMovies,
 });
 export default Data();
