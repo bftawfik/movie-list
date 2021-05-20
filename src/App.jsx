9c8221b0c30a950def29517388cb4a2ccd3e2860
Bishoy Farouk <bftawfik@gmail.com>
2021-05-20 13:15:08 +0200

    loadMovies and pass it to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 
 import Home from "./Pages/Home/Home";
@@ -6,16 +5,44 @@ import Home from "./Pages/Home/Home";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 
-import "./App.scss";
+import Data from "./Services/Data";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header />
-      <Home />
-      <Footer />
-    </React.Fragment>
-  );
+import "./App.scss";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { pageNo: 0, totalPages: 0, totalMovies: 0, loadedMovies: [] };
+  }
+
+  async componentDidMount() {
+    const moviesData = await Data.loadMovies(2);
+    // console.log(moviesData);
+
+    const { page, total_pages, total_results, results } = moviesData;
+    // console.log(page, total_pages, total_results, results);
+    this.setState({
+      pageNo: page,
+      totalPages: total_pages,
+      totalMovies: total_results,
+      loadedMovies: results,
+    });
+  }
+
+  render() {
+    const { pageNo, totalPages, totalMovies, loadedMovies } = this.state;
+    return (
+      <React.Fragment>
+        <Header />
+        <Home
+          pageNo={pageNo}
+          totalPages={totalPages}
+          totalMovies={totalMovies}
+          loadedMovies={loadedMovies}
+        />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
