6417b6f54e2aa30e51ca88a58b27eefa4bef4765
Bishoy Farouk <bftawfik@gmail.com>
2021-05-23 12:50:02 +0200

    pass handlers to form

--- src/Components/AddItemData/AddItemData.jsx
+++ src/Components/AddItemData/AddItemData.jsx
@@ -7,12 +7,12 @@ import { joinClassesWithSpace } from "../../Helpers/helperFunctions";
 
 import * as styles from "./AddItemData.module.scss";
 
-const AddItemData = ({ title, form: { rows }, tempNewMovie, onInput }) => {
-  console.log(rows);
+const AddItemData = ({ title, form: { rows }, tempNewMovie, onInputHandler, onSubmitHandler }) => {
+
   return (
     <div className={styles.AddItemData}>
       <SectionHeader title={title} />
-      <form className={styles.addItemForm}>
+      <form className={styles.addItemForm} onSubmit={onSubmitHandler}>
         {rows.map((row, ndx) => {
           const { type } = row;
           return (
@@ -21,13 +21,13 @@ const AddItemData = ({ title, form: { rows }, tempNewMovie, onInput }) => {
               row={row}
               styles={styles}
               value={tempNewMovie[type]}
-              inputFunction={(e) => onInput(type, e)}
+              inputFunction={(e) => onInputHandler(type, e)}
             />
           );
         })}
         <div className={joinClassesWithSpace(styles.formRow, styles.fullWidth)}>
-          <button>Add Movie</button>
-          <button>Cencel</button>
+          <button type="submit">Add Movie</button>
+          <button type="reset">Cencel</button>
         </div>
       </form>
     </div>
