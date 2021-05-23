a41a49ab1157029298381634fd62c91aa852df83
Bishoy Farouk <bftawfik@gmail.com>
2021-05-23 12:51:27 +0200

    add addItemData mocks

--- src/Mock/home.js
+++ src/Mock/home.js
@@ -7,6 +7,29 @@ const home = {
     title: "My Movies",
     emptyListMsg: "No Movies To Show !",
   },
+  addItemData: {
+    title: "Add a new movie",
+    form: {
+      rows: [
+        {
+          type: "movieName",
+          label: "Name",
+        },
+        {
+          type: "movieDate",
+          label: "Release Date",
+        },
+        {
+          type: "movieOverview",
+          label: "Overview",
+        },
+        {
+          type: "moviePoster",
+          label: "Poster",
+        },
+      ],
+    },
+  },
 };
 
 export default home;
